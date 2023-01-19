4da8f0406df6f8f44a9e3c6cb19ba7394a4f3cc8
John Verrone <john.verrone@gmail.com>
2023-01-18 21:06:11 -0600

    Upgrade SvelteKit to 1.0 (#4)

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
 // import type { Database } from './supabaseTypes';
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+console.log('supabase url', SUPABASE_URL);
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
