1fa4aa48256b60fd43581f77210f1e3db794c708
John Verrone <john.verrone@gmail.com>
2023-05-18 09:14:19 -0600

    Fix redirect loop

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,10 @@
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 import type { PageLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
-export const load = (() => {
+export const load = (async (event) => {
+	const { session } = await getSupabase(event);
+	if (!session) return;
+
 	throw redirect(307, '/trips');
 }) satisfies PageLoad;
