ab918b45bed2204972af8f513c712a1d1a218121
John Verrone <john.verrone@gmail.com>
2023-08-16 20:50:43 -0600

    Add course and tee boxes (#28)

--- src/api/courses.ts
+++ src/api/courses.ts
@@ -2,8 +2,79 @@ import type { TypedSupabaseClient } from '@supabase/auth-helpers-sveltekit';
 import { error } from '@sveltejs/kit';
 import type { ArrayElement, Prettify } from '../utils/typeHelpers';
 
+interface TeeBoxConfig {
+	name: string;
+	rating: number;
+	slope: number;
+}
+
+interface HoleInfo {
+	holeNumber: number;
+	par: number;
+	yardage: number;
+	handicap: number;
+}
+
 export function coursesAPI(supabaseClient: TypedSupabaseClient) {
 	return {
+		/**
+		 * Create new course
+		 */
+		createCourse: async function ({ name }: { name: string }) {
+			const { data, error: dbError } = await supabaseClient
+				.from('courses')
+				.insert({
+					name
+				})
+				.select('id')
+				.limit(1)
+				.single();
+
+			if (dbError) return { error: dbError.message };
+			return { data };
+		},
+
+		/**
+		 * Create tee box
+		 */
+		createTeeBox: async function (
+			courseId: number,
+			teeBoxConfig: TeeBoxConfig,
+			holeInfo: HoleInfo[]
+		) {
+			if (holeInfo.length !== 9 && holeInfo.length !== 18) {
+				throw error(400, { message: 'Incomplete hole form' });
+			}
+
+			const { data, error: teeBoxDbError } = await supabaseClient
+				.from('tee_boxes')
+				.insert({
+					course_id: courseId,
+					name: teeBoxConfig.name,
+					rating: teeBoxConfig.rating,
+					slope: teeBoxConfig.slope
+				})
+				.select('id')
+				.limit(1)
+				.single();
+
+			if (teeBoxDbError) return { error: teeBoxDbError.message };
+			const teeBoxId = data.id;
+
+			const { error: holesError } = await supabaseClient.from('hole_info').insert(
+				holeInfo.map(({ holeNumber, par, yardage, handicap }) => ({
+					tee_box_id: teeBoxId,
+					hole_number: holeNumber,
+					par,
+					yardage,
+					handicap
+				}))
+			);
+
+			if (holesError) return { error: holesError.message };
+			return { success: true };
+		},
+
 		/**
 		 * Get names of all courses
 		 */
--- src/routes/courses/[courseId]/+page.svelte
+++ src/routes/courses/[courseId]/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import Breadcrumbs from '@components/Breadcrumbs.svelte';
 	import BreadcrumbItem from '@components/BreadcrumbItem.svelte';
-	import Main from '@components/Main.svelte';
-	import PageTitle from '@components/PageTitle.svelte';
 	import type { PageData } from './$types';
+	import IconText from '@components/IconText.svelte';
 
 	export let data: PageData;
 
@@ -18,45 +17,50 @@
 	$: selectedTeeBox = selectedTeeBoxId ? teeBoxesById[selectedTeeBoxId] : null;
 </script>
 
-<PageTitle>Courses</PageTitle>
+<Breadcrumbs>
+	<BreadcrumbItem href="/settings" label="Settings" />
+	<BreadcrumbItem href="/courses" label="Courses" />
+	<BreadcrumbItem label={data.course.name} />
+</Breadcrumbs>
 
-<Main>
-	<Breadcrumbs>
-		<BreadcrumbItem href="/settings" label="Settings" />
-		<BreadcrumbItem href="/courses" label="Courses" />
-		<BreadcrumbItem label={data.course.name} />
-	</Breadcrumbs>
+<select class="tee-box-select" bind:value={selectedTeeBoxId}>
+	<option value={undefined}>Select a tee box</option>
+	{#each data.course['tee_boxes'] as teeBox}
+		<option value={teeBox.id}>{teeBox.name} ({teeBox.rating} / {teeBox.slope})</option>
+	{/each}
+</select>
 
-	<select class="tee-box-select" bind:value={selectedTeeBoxId}>
-		<option value={undefined}>Select a tee box</option>
-		{#each data.course['tee_boxes'] as teeBox}
-			<option value={teeBox.id}>{teeBox.name} ({teeBox.rating} / {teeBox.slope})</option>
-		{/each}
-	</select>
+{#if selectedTeeBox}
+	<table class="table">
+		<thead>
+			<th>Hole</th>
+			<th>Par</th>
+			<th>Yardage</th>
+			<th>Handicap</th>
+		</thead>
+		<tbody>
+			{#each selectedTeeBox['hole_info'] as hole}
+				<tr>
+					<td><b>{hole['hole_number']}</b></td>
+					<td>{hole.par}</td>
+					<td>{hole.yardage}</td>
+					<td>{hole.handicap}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+{/if}
 
-	{#if selectedTeeBox}
-		<table class="table">
-			<thead>
-				<th>Hole</th>
-				<th>Par</th>
-				<th>Yardage</th>
-				<th>Handicap</th>
-			</thead>
-			<tbody>
-				{#each selectedTeeBox['hole_info'] as hole}
-					<tr>
-						<td><b>{hole['hole_number']}</b></td>
-						<td>{hole.par}</td>
-						<td>{hole.yardage}</td>
-						<td>{hole.handicap}</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	{/if}
-</Main>
+<a class="add-tee-box-button" href={`/courses/${data.course.id}/createTeeBox`}>
+	<IconText name="plus" label="Add tee box" />
+</a>
 
 <style lang="scss">
+	.add-tee-box-button {
+		display: block;
+		margin-top: 1rem;
+	}
+
 	.table {
 		margin-top: 16px;
 		width: 100%;
