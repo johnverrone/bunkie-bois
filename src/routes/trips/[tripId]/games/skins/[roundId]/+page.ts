a2e60b8ef829bc2ec842dfb3f2b9b279e242f3f6
John Verrone <john.verrone@gmail.com>
2023-10-03 20:03:06 -0600

    cleanup and titles'

--- src/routes/trips/[tripId]/games/skins/[roundId]/+page.ts
+++ src/routes/trips/[tripId]/games/skins/[roundId]/+page.ts
@@ -7,12 +7,13 @@ export const load = (async (event) => {
 	const { getSkinsForRound } = await makeSupabaseAPI(event);
 
 	const roundId = +params.roundId;
+	const round = rounds.find((round) => round.id === roundId);
 
 	const skins = (await getSkinsForRound(roundId)) ?? new Map<string, string[]>();
 
 	return {
-		title: `${title} | Skins`,
-		round: rounds.find((round) => round.id === roundId),
+		title: `${title} | ${round?.name} | Skins`,
+		round,
 		skins
 	};
 }) satisfies PageLoad;
