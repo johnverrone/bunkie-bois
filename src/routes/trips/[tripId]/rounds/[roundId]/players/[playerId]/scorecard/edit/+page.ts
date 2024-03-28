8b85542dd60129d11349d1152df3b31af8fa35e3
John Verrone <john.verrone@gmail.com>
2024-03-27 18:56:02 -0600

    remove more supabase

--- src/routes/trips/[tripId]/rounds/[roundId]/players/[playerId]/scorecard/edit/+page.ts
+++ src/routes/trips/[tripId]/rounds/[roundId]/players/[playerId]/scorecard/edit/+page.ts
@@ -3,7 +3,7 @@ import type { PageLoad } from './$types';
 import { getCourseDetails } from '$lib/api';
 
 export const load = (async (event) => {
-	const { params, parent } = event;
+	const { params, parent, fetch } = event;
 	const { round, tripPlayers } = await parent();
 
 	const player = tripPlayers.find((p) => p.id === params.playerId);
@@ -12,7 +12,7 @@ export const load = (async (event) => {
 	const courseId = round.expand?.course?.id;
 	if (!courseId) throw error(500, 'Unable to get course information.');
 
-	const courseData = await getCourseDetails(courseId);
+	const courseData = await getCourseDetails(courseId, { fetch });
 
 	return {
 		title: `${player.name} Scorecard`,
