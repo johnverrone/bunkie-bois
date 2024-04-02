fe6122922d265a37db6bb0a1d710c4c158dc4418
John Verrone <john.verrone@gmail.com>
2024-04-01 18:13:56 -0600

    fix types and only support adding new players (#62)

--- src/additional-svelte-jsx.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace svelte.JSX {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	interface DOMAttributes<T> {
-		onoutclick?: MouseEventHandler<T> | undefined | null;
-	}
-}
