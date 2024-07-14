795c3ce4aa807821c6b3ab1f6e34241ec692c93d
John Verrone <john.verrone@gmail.com>
2024-07-14 15:29:10 -0600

    more action menu fixes and settings page layout

---
+++ src/routes/settings/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Main from '$lib/components/Main.svelte';
+	import PageTitle from '$lib/components/PageTitle.svelte';
+</script>
+
+<PageTitle>Settings</PageTitle>
+
+<Main>
+	<slot />
+</Main>
