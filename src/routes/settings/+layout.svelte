114f08be1a1e490431aa5f114226ec7e62141717
John Verrone <john.verrone@gmail.com>
2024-07-17 17:24:08 -0600

    fix slot errors

--- src/routes/settings/+layout.svelte
+++ src/routes/settings/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import Main from '$lib/components/Main.svelte';
 	import PageTitle from '$lib/components/PageTitle.svelte';
+
+	let { children } = $props();
 </script>
 
 <PageTitle>Settings</PageTitle>
 
 <Main>
-	<slot />
+	{@render children()}
 </Main>
