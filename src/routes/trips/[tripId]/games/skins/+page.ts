ccfb6ea79ca6f215d79bee696a011c6481ebc3fd
John Verrone <john.verrone@gmail.com>
2024-08-20 16:23:23 -0600

    remove console log

--- src/routes/trips/[tripId]/games/skins/+page.ts
+++ src/routes/trips/[tripId]/games/skins/+page.ts
@@ -6,13 +6,13 @@ export const load = (async ({ parent, fetch }) => {
 
 	const cummulativeSkins = new Map<string, number>();
 	for (const round of rounds) {
-		console.log('getting skins for round', round.id);
 		const skins = await getSkinsForRound(round.id, { fetch });
 		if (!skins) continue;
 		for (const [player, s] of skins) {
 			cummulativeSkins.set(player, (cummulativeSkins.get(player) ?? 0) + s.length);
 		}
 	}
+
 	return {
 		title: title ? `${title} | Skins` : 'Skins',
 		cummulativeSkins
