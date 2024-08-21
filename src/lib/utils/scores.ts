ca4f321f64f4d6e5282a0b3c07b4a24cb4ea14a4
John Verrone <john.verrone@gmail.com>
2024-08-20 20:26:03 -0600

    export scores (#71)

--- src/lib/utils/scores.ts
+++ src/lib/utils/scores.ts
@@ -1,6 +1,22 @@
-import type { HoleScore } from '$lib/pocketbase';
+import type { HoleScore, Scorecard } from '$lib/pocketbase';
 
 export function getScore(holeScores: HoleScore[] | undefined) {
 	if (!holeScores) return 0;
 	return holeScores.reduce((acc, curr) => acc + curr.score, 0);
+}
+
+export function scoresToCsv(scorecards: Scorecard[]): string {
+	const header =
+		'player,' +
+		scorecards[0]?.expand?.holeScores_via_scorecard.map((hole) => hole.holeNumber).join(',');
+	const scores = scorecards
+		.map(
+			(sc) =>
+				sc.expand?.player?.name +
+				',' +
+				sc.expand?.holeScores_via_scorecard.map((hole) => hole.score).join(',')
+		)
+		.join('\r\n');
+
+	return header + '\r\n' + scores;
 }
