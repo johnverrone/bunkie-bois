2ae5ad52ef7dbf06467f5fea59cc59cdf595d5d3
John Verrone <john.verrone@gmail.com>
2024-08-23 16:30:17 -0600

    fix eslint and type error (#73)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,9 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/prettier', 'prettier'],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
